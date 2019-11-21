2dc6f295c707798f7789b4194a53b758bc89e77d
Kari Ikonen <mr.kari.ikonen@gmail.com>
2019-11-21 21:32:04 +0200

    FIX typo in dist

--- rollup.config.js
+++ rollup.config.js
@@ -50,7 +50,7 @@ export default {
 
         // creates `main.css` and `main.css.map` — pass `false`
         // as the second argument if you don't want the sourcemap
-        css.write('dist/typeashead.css');
+        css.write('dist/typeahead.css');
       },
 
       // Warnings are normally passed straight to Rollup. You can
